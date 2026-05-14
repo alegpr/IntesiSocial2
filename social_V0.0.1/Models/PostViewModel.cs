14189637017b7af1860b8b1fce56c1b45e46c5ed
tommasofulgosi <tommaso.fulgosi.studenti@isii.it>
2026-05-14 10:59:05 +0200

    pulizia codice

--- social_V0.0.1/Models/PostViewModel.cs
+++ social_V0.0.1/Models/PostViewModel.cs
@@ -3,13 +3,13 @@
     public class PostViewModel
     {
         public int PostId { get; set; }
-        public string Contenuto { get; set; }
+        public string Contenuto { get; set; } = string.Empty;
         public DateTime DataPubblicazione { get; set; }
         public int LikeCount { get; set; }
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public string Dipartimento { get; set; }
-        public string FotoUrl { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cognome { get; set; } = string.Empty;
+        public string Dipartimento { get; set; } = string.Empty;
+        public string FotoUrl { get; set; } = string.Empty;
         public string Iniziali => $"{(Nome?.Length > 0 ? Nome[0] : "")}{(Cognome?.Length > 0 ? Cognome[0] : "")}";
     }
 }
